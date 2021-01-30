85c9ba8cfa0a3bb13a8795ee74df5a314da6ebbc
Ramazan KARABULUT <32045100+rmznkrblt@users.noreply.github.com>
2021-01-30 19:24:39 +0300

    OOP-introduction

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        //encapsulation
+        public void Add(Product product)//101
+        {
+            Console.WriteLine(product.ProductName + " edded.");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " updated.");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # OOP-csharp
  Object Oriented Programming in C# 
+
+ >to-do
+ 1. methot
+ 2. encapsulation
+ 3. class
